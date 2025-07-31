4a5184546dbc387a789f9ed757fb58e63aee522b
Mithra2008 <mithrak102008@gmail.com>
2025-07-31 14:29:25 +0530

    Create odd or even.py
    odd or even

---
+++ odd or even.py
@@ -0,0 +1,5 @@
+placement_training=["Digital foundationand tech skills","python","python variables","operators in python"]
+for i in placement_training:
+    print(i)
+for i,placement_training in enumerate(placement_training):
+    print(i+1,placement_training)
