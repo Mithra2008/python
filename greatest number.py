0ce43ba88c72c0f1d44e0d5c2032fd9cdeeb587c
Mithra2008 <mithrak102008@gmail.com>
2025-07-31 14:52:37 +0530

    Create greatest number.py
    greatest

---
+++ greatest number.py
@@ -0,0 +1,9 @@
+A=int(input("Enter a number1:"))
+B=int(input("Enter a number2:"))
+C=int(input("Enter a number3:"))
+if A>B:
+    print("A is greatest number")
+elif B>C:
+    print("B is greatest number")
+else:
+    print("C is greatest number")
